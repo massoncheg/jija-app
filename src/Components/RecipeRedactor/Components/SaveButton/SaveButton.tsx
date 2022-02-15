beb26fa8d5d08b4282dba77a1a8d08625d458f58
Massoncheg <massoncheg@gmail.com>
2022-02-15 14:55:57 +0300

    regular commit

--- src/Components/RecipeRedactor/Components/SaveButton/SaveButton.tsx
+++ src/Components/RecipeRedactor/Components/SaveButton/SaveButton.tsx
@@ -1,30 +1,44 @@
 
+import { current } from "@reduxjs/toolkit";
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import saveRecipeToLocalStorage from "../../../../Common/saveRecipeToLocalStorage";
 import { handleNameChange } from "../../../../Store/Slices/commonSlice";
 import { handleRecipeAdd } from "../../../../Store/Slices/MyRecipes/myRecipesSlice";
-import { RootState } from "../../../../Store/store";
+import { handleSubmit } from "../../../../Store/Slices/RecipeRedactor/descriptionSlice";
+import { RecipeState, RootState } from "../../../../Store/store";
 
 import cls from "../RedactorComponents.module.css"
 
+interface SaveButtonProps {
+    state: RecipeState;
+}
 
+const SaveButton = React.memo(({ state }: SaveButtonProps) => {
 
-const SaveButton = () => {
-
-    const state: RootState = useSelector((state: RootState) => state)
     
+
     const dispatch = useDispatch();
 
-    
 
     return (
         <div className={cls.saveButtonWrapper}>
-            <input type="text" onChange={(e) => dispatch(handleNameChange(e))} value={state.common.RecipeName} />
-            <button className={cls.saveButton} onClick={() => {saveRecipeToLocalStorage(state); dispatch(handleRecipeAdd(state))}}>Сохранить изменения</button>
+            <input type="text" onChange={(event) => dispatch(handleNameChange(event.currentTarget.value))} value={state.common.RecipeName} />
+            <button className={cls.saveButton}
+                onClick={() => {
+                    // Меняем состояние: из текущих пропорций генерируем рецепт 
+                    dispatch(handleSubmit({ baseState: state.base, flavoringsState: state.flavorings }))
+                    // Используем состояние, но оно не изменено тк стейт обновится только после ререндера :( 
+                    saveRecipeToLocalStorage(state);
+                    dispatch(handleRecipeAdd(state))
+                }
+                }   
+            >
+                Сохранить изменения
+            </button>
         </div>
     )
-}
+})
 
 export default SaveButton
 
